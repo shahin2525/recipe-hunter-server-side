60b4c0ddf718de0691708613f5fb9e587fdfba64
shahin2525 <shahin451999@gmail.com>
2023-06-03 18:01:09 -0700

    route added in js file

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const port = process.env.PORT || 3000;
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-// const chefs = require("./data.json");
-const chefs = require("./data.json");
-app.get("/chefs", (req, res) => {
-  res.send(chefs);
-});
+
 app.get("/", (req, res) => {
   res.send("Hello World Bangladesh!");
 });
 
-// app.get("/chefs", (req, res) => {
-//   res.send(chefs);
-// });
+const chefs = require("./data.json");
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+// app.get("/news/:id", (req, res) => {
+//     const id = req.params.id;
+//     const selectedId = news.find((n) => n._id === id);
+//     res.send(selectedId);
+//   });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
