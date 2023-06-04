9f91f02d54a8fb2e71a0d88c1cf0884632f07acd
shahin2525 <shahin451999@gmail.com>
2023-06-03 17:52:20 -0700

    chefs data added

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+// const chefs = require("./data.json");
+const chefs = require("./data.json");
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
 app.get("/", (req, res) => {
   res.send("Hello World Bangladesh!");
 });
 
+// app.get("/chefs", (req, res) => {
+//   res.send(chefs);
+// });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
